5ecd5455a07cc76eb4fdedd3a3c3a1ad18ca6bb8
JorgeGaleano2006 <jgaleano@colegioprovidencia.edu.co>
2025-07-08 16:28:27 -0500

    jum

---
+++ Saint-main/saint-app/src/app/pages/technical-report-bigbag/create-report-bigbag/technical-report-bigbag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-report-bigbag',
+  templateUrl: './technical-report-bigbag.component.html',
+  styleUrls: ['./technical-report-bigbag.component.css']
+})
+export class TechnicalReportBigbagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Saint-main/saint-app/src/app/pages/technical-report-bigbag/view-report-bigbag/view-report-bigbag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-report-bigbag',
+  
+  templateUrl: './view-report-bigbag.component.html',
+  styleUrls: ['./view-report-bigbag.component.css'] 
+})
+export class ViewReportBigbagComponent {}
+
